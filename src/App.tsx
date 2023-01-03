04d38ab0835538d6f76207cea3bf5b8de5e4e15e
atultiwarianj <atultiwarianj@gmail.com>
2023-01-03 12:18:03 +0530

    Some changes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,29 @@ import { useDispatch, useSelector } from 'react-redux';
 import { State } from './state/reducers';
 import { Dispatch } from 'redux';
 
+interface Data{
+  userId: number,
+  id: number,
+  title: string,
+  body: string
+}
 
-function App() {
+
+const App =()=> {
   const { posts, loading } =useSelector((state:State)=> ({...state.data}));
 
   const dispatch:Dispatch<any>=useDispatch();
 
   return (
     <div className="App">
-      <h3>redux thunk</h3>
+      <h3>Redux</h3>
       <button onClick={()=>dispatch(fetchPosts())}> Fetch Post</button>
       {!loading ? (
-        posts.map((post:any)=> <h5>{post.body}</h5>)
+        posts.map((posts:Data)=> <h5>{posts.body}</h5>)
       ):(
         <h3>Loading...</h3>
       )}
+      
     </div>
   );
 }
